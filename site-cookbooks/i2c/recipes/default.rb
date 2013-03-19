97d6398de2382b761a78dbac3e446517920762dc
Yuto Hayamizu <y.hayamizu@gmail.com>
2013-03-20 04:59:22 +0900

    add cookbook: i2c

---
+++ site-cookbooks/i2c/recipes/default.rb
@@ -0,0 +1,24 @@
+#
+# Cookbook Name:: i2c
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+%w! i2c-tools python-smbus !.each do |p|
+  package p do
+    action :upgrade
+  end
+end
+
+group "i2c" do
+  action [:modify]
+  append true
+  members 'pi'
+end
+
+package "python-smbus" do
+  action :upgrade
+end
