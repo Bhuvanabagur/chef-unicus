0598a170ec4a9e42f216ae4cf345730df533f057
Yuto Hayamizu <y.hayamizu@gmail.com>
2013-03-20 20:26:52 +0900

    add cookbook: rsyslog

---
+++ site-cookbooks/rsyslog/recipes/default.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook Name:: rsyslog
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'rsyslog' do
+  action :upgrade
+end
+
+template '/etc/rsyslog.conf' do
+  owner 'root'
+  group 'root'
+  mode 0644
+
+  notifies :restart, 'service[rsyslog]'
+end
+
+service 'rsyslog' do
+  action [:enable]
+  supports :start => true, :stop => true, :restart => true, :reload => true
+end
