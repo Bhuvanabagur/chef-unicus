a29cfff61529fa534c7aa192100cffa95603332f
Yuto Hayamizu <y.hayamizu@gmail.com>
2013-03-21 04:14:01 +0900

    recipe[mount-suzuki]: add backup mount point

--- site-cookbooks/mount-suzuki/recipes/default.rb
+++ site-cookbooks/mount-suzuki/recipes/default.rb
@@ -2,20 +2,23 @@
 # Cookbook Name:: mount
 # Recipe:: default
 #
-# Copyright 2013, YOUR_COMPANY_NAME
+# Copyright 2013, Yuto HAYAMIZU
 #
 # All rights reserved - Do Not Redistribute
 #
 
-media_dir = "/suzuki/media"
+media_dir = '/suzuki/media'
+backup_dir = '/suzuki/backup'
 
 # make mount point
-directory media_dir do
-  action :create
-  owner "root"
-  group "root"
-  mode 0755
-  recursive true
+[media_dir, backup_dir].each do |dir|
+  directory dir do
+    action :create
+    owner "root"
+    group "root"
+    mode 0755
+    recursive true
+  end
 end
 
 mount "suzuki media" do
@@ -23,6 +26,15 @@ mount "suzuki media" do
   device 'suzuki.unicus.ddo.jp:/Multimedia'
   fstype 'nfs'
   mount_point media_dir
+  options 'rw,bg,hard,intr,rsize=4096,wsize=4096,timeo=300,nolock'
+  pass 0
+end
+
+mount "suzuki backup" do
+  action [:enable, :mount]
+  device 'suzuki.unicus.ddo.jp:/Backup'
+  fstype 'nfs'
+  mount_point backup_dir
   options 'rw,bg,hard,intr,rsize=4096,wsize=4096,timeo=300,nolock'
   pass 0
 end
