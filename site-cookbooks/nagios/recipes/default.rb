5193d5025034291b528916b7eb108f103aba953b
Yuto Hayamizu <y.hayamizu@gmail.com>
2013-03-21 03:24:30 +0900

    add cookbook: nagios

---
+++ site-cookbooks/nagios/recipes/default.rb
@@ -0,0 +1,44 @@
+#
+# Cookbook Name:: nagios
+# Recipe:: default
+#
+# Copyright 2013, Yuto HAYAMIZU
+#
+# All rights reserved - Do Not Redistribute
+#
+%w|nagios3 nagios-plugins|.each do |p|
+  package p do
+    action :upgrade
+  end
+end
+
+config_path = "/etc/nagios3/conf.d/"
+
+# delete default config files
+["contacts_nagios2.cfg", "extinfo_nagios2.cfg", "generic-host_nagios2.cfg",
+ "generic-service_nagios2.cfg", "hostgroups_nagios2.cfg", "localhost_nagios2.cfg",
+ "services_nagios2.cfg", "timeperiods_nagios2.cfg"].each do |name|
+  config_file = config_path + name
+  file config_file do
+    action :delete
+    notifies :restart, 'service[nagios3]'
+  end
+end
+
+# install config files
+["contacts.cfg", "extinfo.cfg", "generic-host.cfg",
+ "generic-service.cfg", "hostgroups.cfg", "localhost.cfg",
+ "otherhosts.cfg", "services.cfg", "timeperiods.cfg"].each do |name|
+  config_file = config_path + name
+  template config_file do
+    owner 'root'
+    group 'root'
+    mode 0644
+    notifies :restart, 'service[nagios3]'
+  end
+end
+
+service 'nagios3' do
+  action [:enable, :start]
+  supports :start => true, :stop => true, :restart => true, :reload => true, :status => true
+end
