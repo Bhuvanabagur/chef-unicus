fd1a02be7d23cceccdfd099d4b67d9da52f7d903
Yuto Hayamizu <y.hayamizu@gmail.com>
2013-03-20 23:30:05 +0900

    add cookbook: hostname
    set hostname and fqdn correctly

---
+++ site-cookbooks/hostname/metadata.rb
@@ -0,0 +1,9 @@
+name             'hostname'
+maintainer       'Yuto HAYAMIZU'
+maintainer_email 'y.hayamizu@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures hostname'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+supports ['ubuntu', 'raspbian']
---
+++ site-cookbooks/hostname/recipes/default.rb
@@ -0,0 +1,30 @@
+#
+# Cookbook Name:: hostname
+# Recipe:: default
+#
+# Copyright 2013, Yuto HAYAMIZU
+#
+# All rights reserved - Do Not Redistribute
+#
+
+case node["cookbook-hostname"]["hostname"]
+when "starfish"
+  hosts_source_file = 'starfish.hosts.erb'
+when /\Aichigo[0-9]{2}/
+  hosts_source_file = 'ichigo.hosts.erb'
+else
+  raise RuntimeError.new("Unknown host: #{node["chef-hostname"]}")
+end
+
+template '/etc/hostname' do
+  owner 'root'
+  group 'root'
+  mode 0644
+end
+
+template '/etc/hosts' do
+  source hosts_source_file
+  owner 'root'
+  group 'root'
+  mode 0644
+end
