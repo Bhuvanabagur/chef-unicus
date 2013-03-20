03c98696c5d6523f18eb81fb3538e41532afe1fe
Yuto Hayamizu <y.hayamizu@gmail.com>
2013-03-20 22:05:38 +0900

    recipe[rsyslog]: transfar all logs from ichigo* to starfish

--- site-cookbooks/rsyslog/metadata.rb
+++ site-cookbooks/rsyslog/metadata.rb
@@ -6,4 +6,4 @@ description      'Installs/Configures rsyslog'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-supports 'ubuntu'
+supports ['ubuntu', 'raspbian']
