944b1eb0c209a22d742abced4cb2a20101a0e41f
Yuto Hayamizu <y.hayamizu@gmail.com>
2013-03-25 12:51:00 +0900

    add cookbook: networking

---
+++ site-cookbooks/networking/metadata.rb
@@ -0,0 +1,9 @@
+name             'networking'
+maintainer       'Yuto HAYAMIZU'
+maintainer_email 'y.hayamizu@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures networking'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+supports 'raspbian'
---
+++ site-cookbooks/networking/recipes/ichigo.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook Name:: networking
+# Recipe:: ichigo
+#
+# Copyright 2013, Yuto HAYAMIZU
+#
+# All rights reserved - Do Not Redistribute
+#
+
+unless !node['ipaddr'] || node['ipaddr'] =~ /\d+(\.\d+)/
+  raise RuntimeError.new("Valid IP address is not supplied: #{node['ipaddr'].inspect}")
+end
+
+template '/etc/network/interfaces' do
+  owner 'root'
+  group 'root'
+  mode 0644
+
+  # notifies :restart, 'service[networking]'
+end
+
+service 'networking' do
+  action :enable
+  supports :start => true, :stop => true, :restart => true
+end
+
