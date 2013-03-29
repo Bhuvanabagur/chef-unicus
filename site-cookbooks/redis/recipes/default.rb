2e674c2bc62b8007c6f393a707d862de414bdf5e
Yuto Hayamizu <y.hayamizu@gmail.com>
2013-03-30 01:22:23 +0900

    add cookbook: redis

---
+++ site-cookbooks/redis/recipes/default.rb
@@ -0,0 +1,64 @@
+#
+# Cookbook Name:: redis
+# Recipe:: default
+#
+# Copyright 2013, Yuto HAYAMIZU
+#
+# All rights reserved - Do Not Redistribute
+#
+
+redis_tarball_uri = 'http://redis.googlecode.com/files/redis-2.6.11.tar.gz'
+redis_tarball = "#{Chef::Config[:file_cache_path]}/#{File.basename(redis_tarball_uri)}"
+
+remote_file redis_tarball do
+  source redis_tarball_uri
+end
+
+script 'redis-install' do
+  not_if do
+    return false unless system('which redis-server')
+    return false unless File.basename(redis_tarball_uri) =~ /redis-(\d+\.\d+\.\d+)\.tar\.gz$/
+    uri_version = $~[1]
+    return false unless `redis-server --version` =~ / v=(\d+\.\d+\.\d+) /
+    installed_version = $~[1]
+    uri_version == installed_version
+  end
+  interpreter 'bash'
+  action :run
+  user 'root'
+  cwd File.dirname(redis_tarball)
+  code <<EOS
+rm -rf #{File.dirname(redis_tarball)}/#{File.basename(redis_tarball, '.tar.gz')}
+tar xf #{redis_tarball}
+cd #{File.dirname(redis_tarball)}/#{File.basename(redis_tarball, '.tar.gz')}
+./configure --prefix=/usr/local
+make install
+EOS
+end
+
+directory '/etc/redis' do
+  owner 'root'
+  group 'root'
+  mode 0755
+end
+
+template '/etc/redis/redis.conf' do
+  owner 'root'
+  group 'root'
+  mode 0755
+
+  notifies :restart, 'service[redis-server]'
+end
+
+template '/etc/init.d/redis-server' do
+  owner 'root'
+  group 'root'
+  mode 0755
+
+  notifies :enable, 'service[redis-server]'
+end
+
+service 'redis-server' do
+  action [:enable, :start]
+  supports :start => true, :stop => true, :restart => true, :reload => true, :status => true
+end
