58af869adf1ab2e58e029f6328bdf92a8390a75b
Yuto Hayamizu <y.hayamizu@gmail.com>
2013-03-30 01:15:31 +0900

    add cookbook: basic-gems

---
+++ site-cookbooks/basic-gems/metadata.rb
@@ -0,0 +1,10 @@
+name             'basic-gems'
+maintainer       'Yuto HAYAMIZU'
+maintainer_email 'y.hayamizu@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures basic-gems'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+supports ['ubuntu', 'raspbian']
+
---
+++ site-cookbooks/basic-gems/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: basic-gems
+# Recipe:: default
+#
+# Copyright 2013, Yuto HAYAMIZU
+#
+# All rights reserved - Do Not Redistribute
+#
+
+gem_package 'tw' do
+  
+end
