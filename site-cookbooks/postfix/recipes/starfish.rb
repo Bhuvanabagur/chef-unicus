2fdb58a63d2644799c20c91e8fd87922a82c72ff
Yuto Hayamizu <y.hayamizu@gmail.com>
2013-05-13 20:01:39 +0900

    recipe[postfix]: add transport and authinfo maps

--- site-cookbooks/postfix/recipes/starfish.rb
+++ site-cookbooks/postfix/recipes/starfish.rb
@@ -24,3 +24,43 @@ template '/etc/postfix/main.cf' do
 
   notifies :restart, 'service[postfix]'
 end
+
+template '/etc/postfix/transport' do
+  source 'starfish/transport.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
+
+  notifies :run, 'script[hash-transport-map]', :immediately
+  notifies :restart, 'service[postfix]'
+end
+
+template '/etc/postfix/authinfo' do
+  source 'starfish/authinfo.erb'
+  owner 'root'
+  group 'root'
+  mode 0600
+
+  notifies :run, 'script[hash-authinfo]', :immediately
+  notifies :restart, 'service[postfix]'
+end
+
+script 'hash-transport-map' do
+  action :nothing
+  interpreter "bash"
+  user "root"
+
+  code <<-EOS
+  postmap /etc/postfix/transport
+  EOS
+end
+
+script 'hash-authinfo' do
+  action :nothing
+  interpreter "bash"
+  user "root"
+
+  code <<-EOS
+  postmap /etc/postfix/authinfo
+  EOS
+end
