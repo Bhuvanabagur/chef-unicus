24f773f291f84728c84c149ca6fd12f5a9a694f3
Yuto Hayamizu <y.hayamizu@gmail.com>
2013-07-03 20:13:50 +0900

    add cookbook: postgresql

---
+++ site-cookbooks/postgresql/recipes/default.rb
@@ -0,0 +1,35 @@
+#
+# Cookbook Name:: postgresql
+# Recipe:: default
+#
+# Copyright 2013, Yuto HAYAMIZU
+#
+# All rights reserved - Do Not Redistribute
+#
+
+template '/etc/apt/sources.list.d/pgdg.list' do
+  owner 'root'
+  group 'root'
+  mode 0644
+
+  notifies :run, 'script[add key]'
+end
+
+script 'add key' do
+  action :nothing
+  interpreter 'bash'
+  user 'root'
+  code <<-EOS
+wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | apt-key add -
+apt-get update
+EOS
+end
+
+package 'postgresql-9.2' do
+  action :upgrade
+end
+
+service 'postgresql' do
+  action [:enable, :start]
+  supports :start => true, :stop => true, :restart => true, :status => true
+end
