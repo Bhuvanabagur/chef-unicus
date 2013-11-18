7afd0d55e2d11b04dcb1ca3163491405bd5ea92f
Yuto Hayamizu <y.hayamizu@gmail.com>
2013-11-19 01:54:45 +0900

    recipe[basic-packages]: add some packages

--- site-cookbooks/basic-packages/recipes/default.rb
+++ site-cookbooks/basic-packages/recipes/default.rb
@@ -10,23 +10,63 @@
 networking  = ['hping3',
                'curl',
                'tcpdump']
-development = ['binutils',
-               'strace',
-               'build-essential',
-               'make',
-               'libnfs-dev',
-               'linux-tools',
-               'autotools-dev',
-               'git',
-               'manpages-dev']
+
+development = %w|
+binutils
+strace
+build-essential
+libc6-dev
+make
+libnfs-dev
+linux-tools
+autotools-dev
+git
+manpages-dev
+subversion
+bison
+cdbs
+chrpath
+diffstat
+libbison-dev
+libffi-dev
+libgdbm-dev
+libncurses5-dev
+libncursesw5-dev
+libreadline6-dev
+libruby1.8
+libtinfo-dev
+libxss-dev
+libyaml-dev
+quilt
+ruby1.8
+tcl
+tcl-dev
+tcl8.5-dev
+tk
+tk-dev
+tk8.5-dev
+x11proto-scrnsaver-dev
+libssl-dev
+libexpat1-dev
+libgl1-mesa-dev
+libqt4-dev
+|
+
 misc        = ['lv',
                'bc',
                'mailutils',
                'vim',
                'tmux',
-               'sysstat']
-storage = ['dosfstools',
-           'parted']
+               'sysstat',
+               'python-gpgme',
+               'zsh',
+               'graphviz']
+
+storage = %w|
+dosfstools
+parted
+smartmontools
+|
 
 [*networking,
  *development,
